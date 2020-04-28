60bfeaecd325b4a31c468de114dd520824a4beca
Pavel Irzhavski <pir@veeroute.com>
2020-04-28 23:17:54 +0300

    Add \varepsilon to the version number

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ $('.tabular.menu .item').tab();
 const channel = new BroadcastChannel('math_render_channel');
 channel.onmessage = function ({ data }) { setRender(data); }
 
-$('#rawLatex').keypress(function({ originalEvent }){
-  if (originalEvent && originalEvent.ctrlKey && (originalEvent.keyCode === 13 || originalEvent.keyCode === 10)) {
+$('#rawLatex').keyup(function({ originalEvent }){
+//$('#rawLatex').keypress(function({ originalEvent }){
+//  if (originalEvent && originalEvent.ctrlKey && (originalEvent.keyCode === 13 || originalEvent.keyCode === 10)) {
     const text = $('#rawLatex').val();
     console.log(text);
     channel.postMessage(text);
     setRender(text);
-  }
+//  }
 });
 
 function setRender(text) {
