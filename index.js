de27d11c90cbc8f71ee1fc6c255da05449b78585
Pavel Irzhavski <pir@veeroute.com>
2020-05-11 22:05:59 +0300

    make the first instance to be transmitter and all others to be receivers

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ let typesettingPromise = Promise.resolve();
 
 const paragraphs = [];
 const channel = new BroadcastChannel('math_render_channel');
+const channelGuid = 'ba880a8b-6df5-4c34-a2ea-3bcc6e28aaf2';
+transmitter = true;
+
 channel.onmessage = function ({ data }) { setRender(data); }
+
+channel.postMessage(channelGuid);
 
 $('#rawLatex').keyup(function({ originalEvent }){
     const text = $('#rawLatex').val();
@@ -14,6 +19,20 @@ $('#rawLatex').keyup(function({ originalEvent }){
 });
 
 function setRender(text) {
+  console.info(text);
+
+  if (text == channelGuid) {
+    if (transmitter)
+        channel.postMessage(channelGuid + channelGuid);
+    return;
+  }
+
+  if (text == channelGuid + channelGuid) {
+    transmitter = false;
+    document.getElementsByTagName("a")[0].click();
+    return;
+  }
+
   var paragraphTexts = text.split(/\r?\n\r?\n/g)
   // kill off excess paragraphs
   for (let i = paragraphs.length - 1; paragraphTexts.length <= i ; --i) {
@@ -23,7 +42,7 @@ function setRender(text) {
   // update and add
   for (let i = 0; i < paragraphTexts.length; ++i) {
     if (paragraphs.length <= i)
-      $('#paragraphs').append(`<div id="paragraph_${i}" style="width: 50%; white-space: pre;` + (i == 0 ? `height: 100%; ` : `` ) + `" />`)
+      $('#paragraphs').append(`<div id="paragraph_${i}" style="width: 50%; white-space: pre;` + (i == 0 ? `height: 100%; ` : ``) + `" />`)
     const newText = paragraphTexts[i].trim();
     const needsTypesetting = paragraphs[i] !== newText
     paragraphs[i] = newText
